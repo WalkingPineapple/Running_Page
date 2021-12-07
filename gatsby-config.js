09dd8aee69233a5e5675694aef68c433c3734a3d
inclu@sohu.com <inclu@sohu.com>
2021-12-07 09:17:29 +0800

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,93 @@
+module.exports = {
+  pathPrefix: `/`, // Change to `/running_page` when running on github pages
+  siteMetadata: {
+    siteTitle: '肉仔减肥路',
+    siteUrl: 'https://yihong.run',
+    logo: 'https://s3.bmp.ovh/imgs/2021/12/091dd18e9afcdba0.jpg',
+    description: 'Personal site and blog',
+    navLinks: [
+      {
+        name: 'Blog',
+        url: 'https://stepbysteportwo.cn/',
+      },
+      {
+        name: '原作者',
+        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: './',
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-responsive-iframe',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-widows',
+          'gatsby-remark-external-links',
+          {
+            resolve: 'gatsby-remark-autolink-headers',
+            options: {
+              className: 'header-link',
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        precision: 8,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+  ],
+};
